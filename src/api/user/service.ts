1c458b265b8154e569c50abf8930015bc4789ce9
Sukanta Das <arnobh84@gmail.com>
2023-10-12 20:18:45 +0600

    get profile api updated

--- src/api/user/service.ts
+++ src/api/user/service.ts
@@ -1,21 +1,20 @@
 import { JwtPayload } from 'jsonwebtoken';
 import { IUser } from './interface';
-import { User } from './model';
 import ApiError from '../../error/ApiError';
 import { httpCode } from '../../shared/httpCodes';
+import { prisma } from '../../shared/primsa';
 
 const getUser = async (user: JwtPayload): Promise<IUser | null> => {
     const { email } = user;
-    const profile = await User.findOne({ email });
+    const profile = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
     if (!profile) {
         throw new ApiError(httpCode.NOT_FOUND, 'User not found');
     }
-
-    const userWithoutPassword = await User.findById(
-        profile._id
-    ).select('-password');
-
-    return userWithoutPassword;
+    return profile;
 };
 
 export const UserService = {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { TestRoute } from '../api/test/routes';
 import { AuthRoute } from '../api/auth/routes';
+import { UserRoute } from '../api/user/routes';
 
 const router = express.Router();
 
 router.use('/', TestRoute);
 router.use('/auth', AuthRoute);
-// router.use('/user', UserRoute);
+router.use('/user', UserRoute);
 
 export default router;
