b237feede55a6bb8f33e0a19f762f5e5c23c5d32
Sukanta Das <arnobh84@gmail.com>
2023-10-17 19:10:08 +0600

    new rouute added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import { UserRoute } from '../api/user/routes';
 import { ServiceRoute } from '../api/service/routes';
 import { BookingRoute } from '../api/booking/routes';
 import { ReviewRoute } from '../api/review/routes';
+import { FeedBackRoute } from '../api/feedback/routes';
 
 const router = express.Router();
 
@@ -14,5 +15,6 @@ router.use('/user', UserRoute);
 router.use('/service', ServiceRoute);
 router.use('/booking', BookingRoute);
 router.use('/review', ReviewRoute);
+router.use('/feedback', FeedBackRoute);
 
 export default router;
